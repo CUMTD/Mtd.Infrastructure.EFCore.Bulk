3c751f80d5a5d18a4e4b1ece0b4743a2547f8eab
Ryan Blackman <rblackman@mtd.org>
2025-04-17 08:53:21 -0500

    Make repository methods virtual for extensibility
    Modified method definitions in `AsyncBulkEFIdentifiableRepository`
    and `AsyncBulkEFRepository` to make the following methods `virtual`:
    - `BulkDeleteAsync`
    - `BulkInsertAsync`
    - `BulkInsertOrUpdateAsync`
    - `BulkInsertOrUpdateOrDeleteAsync`
    - `BulkSaveChangesAsync`
    - `BulkUpdateAsync`
    - `TruncateAsync`
    
    These changes allow derived classes to override the methods,
    enhancing extensibility. Method functionality remains unchanged,
    as they still delegate to `_dbContext`.

--- Repository/AsyncBulkEFIdentifiableRepository.cs
+++ Repository/AsyncBulkEFIdentifiableRepository.cs
@@ -14,18 +14,18 @@ public abstract class AsyncBulkEFIdentifiableRepository<T_Identity, T_Entity>(Db
 	where T_Identity : notnull, IComparable<T_Identity>
 	where T_Entity : class, IIdentity<T_Identity>
 {
-	public Task BulkDeleteAsync(IEnumerable<T_Entity> entities, CancellationToken cancellationToken) =>
+	public virtual Task BulkDeleteAsync(IEnumerable<T_Entity> entities, CancellationToken cancellationToken) =>
 		_dbContext.BulkDeleteAsync(entities, cancellationToken: cancellationToken);
-	public Task BulkInsertAsync(IEnumerable<T_Entity> entities, CancellationToken cancellationToken) =>
+	public virtual Task BulkInsertAsync(IEnumerable<T_Entity> entities, CancellationToken cancellationToken) =>
 		_dbContext.BulkInsertAsync(entities, cancellationToken: cancellationToken);
-	public Task BulkInsertOrUpdateAsync(IEnumerable<T_Entity> entities, CancellationToken cancellationToken) =>
+	public virtual Task BulkInsertOrUpdateAsync(IEnumerable<T_Entity> entities, CancellationToken cancellationToken) =>
 		_dbContext.BulkInsertOrUpdateAsync(entities, cancellationToken: cancellationToken);
-	public Task BulkInsertOrUpdateOrDeleteAsync(IEnumerable<T_Entity> entities, CancellationToken cancellationToken) =>
+	public virtual Task BulkInsertOrUpdateOrDeleteAsync(IEnumerable<T_Entity> entities, CancellationToken cancellationToken) =>
 		_dbContext.BulkInsertOrUpdateOrDeleteAsync(entities, cancellationToken: cancellationToken);
-	public Task BulkSaveChangesAsync(CancellationToken cancellationToken) =>
+	public virtual Task BulkSaveChangesAsync(CancellationToken cancellationToken) =>
 		_dbContext.BulkSaveChangesAsync(cancellationToken: cancellationToken);
-	public Task BulkUpdateAsync(IEnumerable<T_Entity> entities, CancellationToken cancellationToken) =>
+	public virtual Task BulkUpdateAsync(IEnumerable<T_Entity> entities, CancellationToken cancellationToken) =>
 		_dbContext.BulkUpdateAsync(entities, cancellationToken: cancellationToken);
-	public Task TruncateAsync(CancellationToken cancellationToken) =>
+	public virtual Task TruncateAsync(CancellationToken cancellationToken) =>
 		_dbContext.TruncateAsync<T_Entity>(cancellationToken: cancellationToken);
 }
--- Repository/AsyncBulkEFRepository.cs
+++ Repository/AsyncBulkEFRepository.cs
@@ -8,18 +8,18 @@ using Mtd.Infrastructure.EFCore.Repositories;
 namespace Mtd.Infrastructure.EFCore.Bulk.Repository;
 public abstract class AsyncBulkEFRepository<T>(DbContext context) : AsyncEFRepository<T>(context), IAsyncBulkWriteable<T, IReadOnlyCollection<T>> where T : class
 {
-	public Task BulkDeleteAsync(IEnumerable<T> entities, CancellationToken cancellationToken) =>
+	public virtual Task BulkDeleteAsync(IEnumerable<T> entities, CancellationToken cancellationToken) =>
 		_dbContext.BulkDeleteAsync(entities, cancellationToken: cancellationToken);
-	public Task BulkInsertAsync(IEnumerable<T> entities, CancellationToken cancellationToken) =>
+	public virtual Task BulkInsertAsync(IEnumerable<T> entities, CancellationToken cancellationToken) =>
 		_dbContext.BulkInsertAsync(entities, cancellationToken: cancellationToken);
-	public Task BulkInsertOrUpdateAsync(IEnumerable<T> entities, CancellationToken cancellationToken) =>
+	public virtual Task BulkInsertOrUpdateAsync(IEnumerable<T> entities, CancellationToken cancellationToken) =>
 		_dbContext.BulkInsertOrUpdateAsync(entities, cancellationToken: cancellationToken);
-	public Task BulkInsertOrUpdateOrDeleteAsync(IEnumerable<T> entities, CancellationToken cancellationToken) =>
+	public virtual Task BulkInsertOrUpdateOrDeleteAsync(IEnumerable<T> entities, CancellationToken cancellationToken) =>
 		_dbContext.BulkInsertOrUpdateOrDeleteAsync(entities, cancellationToken: cancellationToken);
-	public Task BulkSaveChangesAsync(CancellationToken cancellationToken) =>
+	public virtual Task BulkSaveChangesAsync(CancellationToken cancellationToken) =>
 		_dbContext.BulkSaveChangesAsync(cancellationToken: cancellationToken);
-	public Task BulkUpdateAsync(IEnumerable<T> entities, CancellationToken cancellationToken) =>
+	public virtual Task BulkUpdateAsync(IEnumerable<T> entities, CancellationToken cancellationToken) =>
 		_dbContext.BulkUpdateAsync(entities, cancellationToken: cancellationToken);
-	public Task TruncateAsync(CancellationToken cancellationToken) =>
+	public virtual Task TruncateAsync(CancellationToken cancellationToken) =>
 		_dbContext.TruncateAsync<T>(cancellationToken: cancellationToken);
 }
